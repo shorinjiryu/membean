cd8263519a7990b8a05f7ea9fc5bd425719ff3c4
Shorin <thomasshorin@gmail.com>
2015-02-27 15:02:28 -0800

    Update shit.js
    formatting

--- shit.js
+++ shit.js
@@ -2,11 +2,53 @@
 function start(){
   document.getElementById("start-button")&&(console.log("Start")),
   document.getElementById("start-button").click());
+  
 }
 
 function Initiate(){
   document.getElementById("Proceed")&&document.getElementById("Proceed").click();
+  
 }
 
 function runtime(){
-  document.getElementById("user-growl")&&("Restudy"==document.getElementById("user-growl").innerHTML||"Study"==document.getElementById("user-growl").innerHTML)?(console.log("Answering Review"),document.getElementsByClassName("choice answer")[0].click(),document.getElementById("next-btn").click()):(console.log("Answering Normal"),document.getElementById("pass__pass").click())}var A=27e3;!function(){start(),setTimeout(Initiate,2500),setTimeout(runtime,A),setTimeout(runtime,2*A+10),setTimeout(runtime,3*A),setTimeout(runtime,4*A),setTimeout(runtime,5*A),setTimeout(runtime,6*A),setTimeout(runtime,7*A),setTimeout(runtime,8*A+10),setTimeout(runtime,9*A),setTimeout(runtime,10*A),setTimeout(runtime,11*A),setTimeout(runtime,12*A),setTimeout(runtime,13*A),setTimeout(runtime,14*A),setTimeout(runtime,15*A+10),setTimeout(runtime,16*A),setTimeout(runtime,17*A),setTimeout(runtime,18*A),setTimeout(runtime,19*A),setTimeout(runtime,20*A),setTimeout(runtime,21*A),setTimeout(runtime,22*A),setTimeout(runtime,23*A+10),setTimeout(runtime,24*A),setTimeout(runtime,25*A),setTimeout(runtime,26*A),setTimeout(runtime,27*A),setTimeout(runtime,28*A),setTimeout(runtime,29*A),setTimeout(runtime,30*A),setTimeout(runtime,31*A+10),setTimeout(runtime,32*A),setTimeout(runtime,33*A),setTimeout(runtime,34*A),setTimeout(runtime,35*A),setTimeout(runtime,36*A)}();
+  document.getElementById("user-growl")&&("Restudy"==document.getElementById("user-growl").innerHTML||"Study"==document.getElementById("user-growl").innerHTML)?(console.log("Answering Review"),
+  document.getElementsByClassName("choice answer")[0].click(),
+  document.getElementById("next-btn").click()):(console.log("Answering Normal"),
+  document.getElementById("pass__pass").click())
+  
+}
+var A=27e3;!function(){
+  start(),
+  setTimeout(Initiate,2500),
+  setTimeout(runtime,A),
+  setTimeout(runtime,2*A+10),
+  setTimeout(runtime,3*A),
+  setTimeout(runtime,4*A),
+  setTimeout(runtime,5*A),
+  setTimeout(runtime,6*A),
+  setTimeout(runtime,7*A),
+  setTimeout(runtime,8*A+10),
+  setTimeout(runtime,9*A),
+  setTimeout(runtime,10*A),
+  setTimeout(runtime,11*A),
+  setTimeout(runtime,12*A),
+  setTimeout(runtime,13*A),
+  setTimeout(runtime,14*A),
+  setTimeout(runtime,15*A+10),
+  setTimeout(runtime,16*A),
+  setTimeout(runtime,17*A),
+  setTimeout(runtime,18*A),
+  setTimeout(runtime,19*A),
+  setTimeout(runtime,20*A),
+  setTimeout(runtime,21*A),
+  setTimeout(runtime,22*A),
+  setTimeout(runtime,23*A+10),
+  setTimeout(runtime,24*A),
+  setTimeout(runtime,25*A),
+  setTimeout(runtime,26*A),
+  setTimeout(runtime,27*A),
+  setTimeout(runtime,28*A),
+  setTimeout(runtime,29*A),
+  setTimeout(runtime,30*A),
+  setTimeout(runtime,31*A+10),
+  setTimeout(runtime,32*A),setTimeout(runtime,33*A),setTimeout(runtime,34*A),setTimeout(runtime,35*A),setTimeout(runtime,36*A)}();
